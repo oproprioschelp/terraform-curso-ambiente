e73f6972f2286684e8f1b0ddeeab074639f2e223
Guilherme Schelp <oproprioschelp@gmail.com>
2022-10-11 20:13:27 -0300

    db_subnet

--- main.tf
+++ main.tf
@@ -32,3 +32,26 @@ module "vpc" {
   vpc_cidr  = "172.32.0.0/16"
   nat_count = 2
 }
+
+module "rds" {
+  source               = "git@github.com:oproprioschelp/terraform-aws-rds.git"
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "schelp"
+  password             = "gotf"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  db_subnet            = aws_db_subnet_group.default.id
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
